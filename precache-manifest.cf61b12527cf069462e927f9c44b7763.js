6756cbffd7e24d44587ce4bc4c8e2998cf006278
theDavidBarton <43063460+theDavidBarton@users.noreply.github.com>
2020-01-30 19:20:06 +0000

    Deploying to gh-pages from master 78602bcf7dae3d94a240184603fecb9ac137f3ff

---
+++ precache-manifest.cf61b12527cf069462e927f9c44b7763.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1960b838d9b9603ec877d69d4e3524a6",
+    "url": "/video-games-on-RAWG-react-app/index.html"
+  },
+  {
+    "revision": "6d750738c494e37ecdf6",
+    "url": "/video-games-on-RAWG-react-app/static/css/2.903a27b1.chunk.css"
+  },
+  {
+    "revision": "158d27a8f0b5a91715ac",
+    "url": "/video-games-on-RAWG-react-app/static/css/main.58bd7652.chunk.css"
+  },
+  {
+    "revision": "6d750738c494e37ecdf6",
+    "url": "/video-games-on-RAWG-react-app/static/js/2.66651fbb.chunk.js"
+  },
+  {
+    "revision": "158d27a8f0b5a91715ac",
+    "url": "/video-games-on-RAWG-react-app/static/js/main.18310387.chunk.js"
+  },
+  {
+    "revision": "b8392a2f119cfabd1890",
+    "url": "/video-games-on-RAWG-react-app/static/js/runtime~main.cd021995.js"
+  },
+  {
+    "revision": "5a94f70d047866186a5624c41eff46b6",
+    "url": "/video-games-on-RAWG-react-app/static/media/ArcadeClassic.5a94f70d.otf"
+  },
+  {
+    "revision": "07883e93734b98cae0f7b9c55d287250",
+    "url": "/video-games-on-RAWG-react-app/static/media/github.07883e93.svg"
+  },
+  {
+    "revision": "fc21cea151f5aa0f837e6c2dd30306fb",
+    "url": "/video-games-on-RAWG-react-app/static/media/linkedin.fc21cea1.png"
+  },
+  {
+    "revision": "e4d15341d738fd7fb486171ebc27973a",
+    "url": "/video-games-on-RAWG-react-app/static/media/logo-big-dark.e4d15341.svg"
+  },
+  {
+    "revision": "31734e48bb7ba3884b5edd0c67c4410f",
+    "url": "/video-games-on-RAWG-react-app/static/media/logo-big.31734e48.svg"
+  }
+]);
